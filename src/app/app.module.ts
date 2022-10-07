c10c5153912f136bbdab2c47654f14f9ebebc2ae
Petar <pmarkovic2308@gmail.com>
2022-10-08 01:24:47 +0200

    Finished cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,10 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
-import { HomepageComponent } from './pages/homepage/homepage.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { CardComponent } from './components/card/card.component';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppComponent} from './app.component';
+import {HomepageComponent} from './pages/homepage/homepage.component';
+import {NavbarComponent} from './components/navbar/navbar.component';
+import {FooterComponent} from './components/footer/footer.component';
+import {CardComponent} from './components/card/card.component';
 
 @NgModule({
   declarations: [
@@ -21,4 +20,5 @@ import { CardComponent } from './components/card/card.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
